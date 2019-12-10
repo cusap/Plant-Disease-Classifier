9680ca7394d6186b766e59689d179f3dfbb09c09
Minh-Thai <minhtyyufa@gmail.com>
2019-12-10 14:55:43 -0500

    fixed opt

--- disease_classifier.py
+++ disease_classifier.py
@@ -3,16 +3,19 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 import glob
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, GlobalAveragePooling2D ,BatchNormalization, Add, \
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, AveragePooling2D ,BatchNormalization, Add, \
     DepthwiseConv2D, ReLU, Reshape
 from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint
-
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 PERCENT_TRAIN = .8
 lamb = 0
-path_to_parent = r"/home/winnie/dhvanil/cgml/plant-classifier"
-segmented_path = path_to_parent + r"\PlantVillage-Dataset\raw\segmented\*"
+#path_to_parent = r"/home/winnie/dhvanil/cgml/plant-classifier"
+path_to_parent = r"C:\Users\minht\PycharmProjects\Deep Learning\final_proj"
+segmented_path = path_to_parent + r"\PlantVillage-Dataset\raw\segmented\Apple*"
 learning_rate = .045
+#learning_rate = .1
 lr_decay = .98
 batch_size = 32
 epochs = 200
@@ -42,7 +45,9 @@ def open_data():
         if i == 34:
             continue
         for count, pic_name in enumerate(glob.glob(label_name + "\\*")):
-            if count%8==0:
+            if count ==2:
+                break
+            if count%16==0:
                 im = Image.open(pic_name)
                 im.load()
                 new_im = np.asarray(im, dtype='float32')
@@ -62,10 +67,14 @@ def get_data():
 def bottleneck_s1(orig, num_channels, exp_fac, resid=True):
     x = Conv2D(orig.get_shape()[-1]*exp_fac, kernel_size=(1,1), strides=1, padding='same',kernel_regularizer=tf.keras.regularizers.l2(lamb),
             activity_regularizer=tf.keras.regularizers.l2(lamb))(orig)
-    x = ReLU(max_value=6)(x)
+    #x = ReLU(max_value=6)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
     x = DepthwiseConv2D(3, padding='same', kernel_regularizer=tf.keras.regularizers.l2(lamb),
             activity_regularizer=tf.keras.regularizers.l2(lamb))(x)
-    x = ReLU(max_value=6)(x)
+    #x = ReLU(max_value=6)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
     x = Conv2D(num_channels, kernel_size=(1,1), strides=1, padding ='same',kernel_regularizer=tf.keras.regularizers.l2(lamb),
             activity_regularizer=tf.keras.regularizers.l2(lamb), activation='linear')(x)
     if(resid):
@@ -76,10 +85,12 @@ def bottleneck_s1(orig, num_channels, exp_fac, resid=True):
 def bottleneck_s2(x, num_channels, exp_fac):
     x = Conv2D(x.get_shape()[-1]*exp_fac, kernel_size=(1, 1), strides=1, padding='same')(x)
     x = BatchNormalization()(x)
-    x = ReLU(max_value=6)(x)
+    x = ReLU()(x)
+    #x = ReLU(max_value=6)(x)
     x = DepthwiseConv2D(3, padding='same', strides= 2)(x)
     x = BatchNormalization()(x)
-    x = ReLU(max_value=6)(x)
+    x = ReLU()(x)
+    #x = ReLU(max_value=6)(x)
     x = Conv2D(num_channels, kernel_size=(1, 1), strides= 1, padding='same', activation='linear')(x)
     return x
 
@@ -101,35 +112,49 @@ def scheduler(epoch):
 
 if __name__ == '__main__':
     train_im, train_labels, val_im, val_labels, label_names = get_data()
-    print(label_names)
     print(len(train_im))
-    print(train_im[1])
     try:
         with tf.device('/device:GPU:0'):
             input = tf.keras.Input(shape=(train_im.shape[1:]))
-            x = Conv2D(32, 3,strides= 2,padding='same', kernel_regularizer=tf.keras.regularizers.l2(lamb),
-                       activity_regularizer=tf.keras.regularizers.l2(lamb))(input)
+            '''
+            #basic
+            x = Conv2D(32,3,3)(input)
+            x = Conv2D(32,3,3)(x)
+            x = Conv2D(32, 3, 3)(x)
+            #x = Conv2D(32, 3, 2)(x)
+            x = Flatten()(x)
+            x = Dense(len(label_names))(x)
+            output = Activation('softmax')(x)
+            '''
+
+            x = Conv2D(32, 3,strides= 2,padding='same')(input)
 
             x = b_block(x, num_channels = 16, exp_fac=1, n=1, s=1)
-            x = b_block(x, num_channels = 24, exp_fac=6, n=2, s=2)
-            x = b_block(x, num_channels = 32, exp_fac=6, n=3, s=2)
-            x = b_block(x, num_channels = 64, exp_fac=6, n=4, s=2)
+            x = b_block(x, num_channels = 48, exp_fac=6, n=2, s=2)
+            x = b_block(x, num_channels = 64, exp_fac=6, n=3, s=2)
+            #x = b_block(x, num_channels = 128, exp_fac=6, n=4, s=2)
 
-            x = Conv2D(160, 1, strides=1, padding='same', kernel_regularizer=tf.keras.regularizers.l2(lamb),
-                       activity_regularizer=tf.keras.regularizers.l2(lamb))(x)
-            x = GlobalAveragePooling2D()(x)
-            x = Reshape((1,1,160))(x)
+            x = Conv2D(64, 1, strides=1, padding='same')(x)
+            print(x.get_shape)
+            x = AveragePooling2D(pool_size=(2,2))(x)
+            #x = Flatten()(x)
+            x = Reshape((1,1,256))(x)
             x = Conv2D(len(label_names),1, padding='same')(x)
+            #x = Dense(len(label_names))(x)
             x = Activation('softmax')(x)
             output = Reshape((len(label_names),))(x)
+
+
+
             model = tf.keras.Model(inputs=input, outputs=output)
 
-
             #save model checkpoints
-            cp_path = path_to_parent + r"Plant-Disease-Classifier\model-checkpoints\{epoch:04d}.cpkt"
+            cp_path = path_to_parent + r"\Plant-Disease-Classifier\model-checkpoints\{epoch:04d}.cpkt"
             cp_dir = os.path.dirname(cp_path)
             cp_callback = ModelCheckpoint(filepath=cp_path, save_weights_only=True, save_best_only=True, period=10,
                                               verbose=1)
+
+            '''
             im_gen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=360,
                                                                      zoom_range=[0.5, 1.0],
                                                                      width_shift_range=0.4,
@@ -140,23 +165,42 @@ if __name__ == '__main__':
                                                                      brightness_range = [.2, 1.0],
                                                                      shear_range = 45,
                                                                      )
+                                                                     '''
+            '''
+            im_gen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=0,
+                                                                     zoom_range=[0.1, .1],
+                                                                     width_shift_range=0.04,
+                                                                     height_shift_range=0.04,
+                                                                     horizontal_flip=True,
+                                                                     vertical_flip=True,
+                                                                     data_format="channels_last",
+                                                                     brightness_range = [.2, 1.0],
+                                                                     )
 
             im_gen.fit(train_im)
+            '''
             lr_scheduler = LearningRateScheduler(scheduler, verbose=1)
 
 
-            opt = tf.compat.v1.train.RMSPropOptimizer(learning_rate, decay=.9, momentum=.9)
+            opt = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, decay=.9, momentum=.9)
 
             model.compile(loss=tf.keras.losses.categorical_crossentropy,
-                          optimizer=tf.keras.optimizers.RMSprop(),
+                          optimizer=opt,
                           metrics=['accuracy'])
             model.summary()
 
 
+            model_log = model.fit(train_im, train_labels, batch_size=batch_size, epochs=epochs,
+                                  callbacks=[lr_scheduler, cp_callback],
+                                  validation_data=(val_im, val_labels), verbose=2)
+
+            '''
             model_log = model.fit_generator(im_gen.flow(train_im, train_labels, batch_size=batch_size),
                                             steps_per_epoch=train_im.shape[0] // batch_size, epochs=epochs,
                                             callbacks=[lr_scheduler, cp_callback],
                                             validation_data=(val_im, val_labels), verbose=2)
+            '''
+            #model_log = model.fit(train_im, train_labels    )
     except RuntimeError as e:
         print(e)
 
